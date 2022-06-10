42c2b664fe7dbd7074f6181e32c036261141eba1
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-06-10 20:17:16 +0900

    feat: Job에 대한 구조 이해

--- src/main/java/com/smalldogg/study/springbatch/JobConfiguration.java
+++ src/main/java/com/smalldogg/study/springbatch/JobConfiguration.java
@@ -14,30 +14,32 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
+public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    //Job에 대해 학습
     @Bean
-    public Job job(){
-        return jobBuilderFactory.get("DBJob")
-                .start(firstStep())
-                .next(secondStep())
+    public Job job2() {
+        return jobBuilderFactory.get("job2")
+                .start(step2_1())
+                .next(step2_2())
                 .build();
     }
 
-    public Step secondStep() {
-        return stepBuilderFactory.get("step1")
+    public Step step2_2() {
+        return stepBuilderFactory.get("step2-2")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">>>step1");
+                    System.out.println("Step2-2 was executed");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
 
-    public Step firstStep() {
-        return stepBuilderFactory.get("step2")
+    @Bean
+    public Step step2_1() {
+        return stepBuilderFactory.get("step2-1")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">>>step2");
+                    System.out.println("Step2-1 was executed");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
